fda893ec92650c23a048e7c40f252f56999e4533
damoayo <mamapai@naver.com>
2026-01-05 22:06:39 +0900

    Initial commit

---
+++ src/main/java/com/tarae/minipostman/MiniPostmanCloneApplication.java
@@ -0,0 +1,13 @@
+package com.tarae.minipostman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPostmanCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPostmanCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarae/minipostman/MiniPostmanCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarae.minipostman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPostmanCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
