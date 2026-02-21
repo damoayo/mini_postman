04aad97978babebb343a66e25ce6755b3812382d
damoayo <mamapai@naver.com>
2026-02-21 13:32:20 +0900

    WebConfig에서 경로변경

--- src/main/java/com/minipostman/tarae/config/WebConfig.java
+++ src/main/java/com/minipostman/tarae/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+		registry.addMapping("/api/**").allowedOrigins("http://localhost:3000")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP 메서드
 				.allowedHeaders("*") // 모든 헤더 허용
 				.allowCredentials(true); // 자격 증명 허용 여부 (쿠키, 인증 정보 등)
