39700e2f719a3cf125f65f5f256a779e8c92c470
damoayo <mamapai@naver.com>
2026-03-01 12:58:08 +0900

    User.java수정

--- src/main/java/com/minipostman/tarae/domain/User.java
+++ src/main/java/com/minipostman/tarae/domain/User.java
@@ -33,4 +33,20 @@ public class User extends BaseEntity {
 		this.name = name;
 		this.email = email;
 	}
+
+	/**
+	 * 사용자 정보 수정 메서드
+	 *
+	 * 학습 포인트: - JPA에서는 엔티티 객체를 직접 수정하면 트랜잭션 끝날 때 자동으로 UPDATE 쿼리 실행 - 이것을 "더티
+	 * 체킹(Dirty Checking)" 이라고 합니다 - Setter를 열지 않고 의미 있는 메서드명(update)을 사용하는 것이 좋은
+	 * 설계입니다
+	 */
+	public void update(String name, String email) {
+		if (name != null) {
+			this.name = name;
+		}
+		if (email != null) {
+			this.email = email;
+		}
+	}
 }
